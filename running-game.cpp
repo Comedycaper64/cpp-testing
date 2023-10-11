dbcbee10afb8d2897bde6acbc5b6440eae4aa1e1
Matvey Shchetinin <mshchetinin64@gmail.com>
2023-10-11 16:21:12 +0100

    Begins runner g ame

---
+++ running-game.cpp
@@ -0,0 +1,16 @@
+// #include "raylib.h"
+
+// int main()
+// {
+//     int windowWidth{800};
+//     int windowHeight{400};
+//     InitWindow(windowWidth, windowHeight, "Runner Game");
+
+//     SetTargetFPS(60);
+//     while (true)
+//     {
+//         BeginDrawing();
+//         ClearBackground(WHITE);
+//         EndDrawing();
+//     }
+// }
--- test.cpp
+++ test.cpp
@@ -1,70 +1,142 @@
-#include <cstdio>
-#include <cmath>
+// #include <cstdio>
+// #include <cmath>
+// #include "raylib.h"
+
+// int main()
+// {
+//     int windowWidth = 800;
+//     int windowHeight = 500;
+
+//     // printf("Hello there");
+//     InitWindow(windowWidth, windowHeight, "Axe Game");
+
+//     int circleX{100};
+//     int circleY{100};
+//     int circleRadius{25};
+//     int circleSpeed{5};
+
+//     int axeX{500};
+//     int axeY{300};
+//     int axeLength{40};
+//     Vector2 axeVelocity{};
+//     int axeMaxSpeed{5};
+//     float axeAcceleration{0.2};
+
+//     SetTargetFPS(60);
+//     while (!WindowShouldClose())
+//     {
+//         BeginDrawing();
+//         ClearBackground(BLUE);
+//         if (IsKeyDown(KEY_D) && (circleX + circleRadius < windowWidth))
+//         {
+//             circleX += circleSpeed;
+//         }
+//         if (IsKeyDown(KEY_A) && (circleX + circleRadius > 0))
+//         {
+//             circleX -= circleSpeed;
+//         }
+//         if (IsKeyDown(KEY_S) && (circleY + circleRadius < windowHeight))
+//         {
+//             circleY += circleSpeed;
+//         }
+//         if (IsKeyDown(KEY_W) && (circleY + circleRadius > 0))
+//         {
+//             circleY -= circleSpeed;
+//         }
+//         DrawCircle(circleX, circleY, circleRadius, RED);
+
+//         Vector2 desiredAxeDirection{circleX - axeX, circleY - axeY};
+//         float directionMagnitude{sqrt(pow(desiredAxeDirection.x, 2) + pow(desiredAxeDirection.y, 2))};
+//         Vector2 normalisedAxeDirection{desiredAxeDirection.x / directionMagnitude, desiredAxeDirection.y / directionMagnitude};
+//         Vector2 acceleratedAxeDirection{normalisedAxeDirection.x * axeAcceleration, normalisedAxeDirection.y * axeAcceleration};
+//         Vector2 temp{acceleratedAxeDirection.x + axeVelocity.x, acceleratedAxeDirection.y + axeVelocity.y};
+//         if (sqrt(pow(temp.x, 2) + pow(temp.y, 2)) < axeMaxSpeed)
+//         {
+//             axeVelocity = temp;
+//         }
+
+//         axeX += axeVelocity.x;
+//         axeY += axeVelocity.y;
+
+//         DrawRectangle(axeX, axeY, axeLength, axeLength, GREEN);
+
+//         if (directionMagnitude < (circleRadius + axeLength / 2))
+//         {
+//             DrawText("Game Over!", 400, 200, 20, RED);
+//         }
+
+//         EndDrawing();
+//     }
+// }
+
 #include "raylib.h"
 
 int main()
 {
-    int windowWidth = 800;
-    int windowHeight = 500;
+    const int windowWidth{800};
+    const int windowHeight{400};
+    InitWindow(windowWidth, windowHeight, "Runner Game");
 
-    // printf("Hello there");
-    InitWindow(windowWidth, windowHeight, "Axe Game");
+    Texture2D characterTexture = LoadTexture("textures/scarfy.png");
+    Rectangle characterRect;
+    characterRect.width = characterTexture.width / 6;
+    characterRect.height = characterTexture.height;
+    characterRect.x = 0;
+    characterRect.y = 0;
 
-    int circleX{100};
-    int circleY{100};
-    int circleRadius{25};
-    int circleSpeed{5};
+    Vector2 characterPos;
+    characterPos.x = windowWidth / 2 - characterRect.width / 2;
+    characterPos.y = windowHeight - characterRect.height;
 
-    int axeX{500};
-    int axeY{300};
-    int axeLength{40};
-    Vector2 axeVelocity{};
-    int axeMaxSpeed{5};
-    float axeAcceleration{0.2};
+    const int gravity = 1000;
+    const int jumpVelocity = -750;
+
+    const float animUpdateTime = 0.1;
+    float animTimeTracker{0};
+    int frame{0};
+
+    // int posY{windowHeight - rectHeight};
+    int velocity{0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
-        if (IsKeyDown(KEY_D) && (circleX + circleRadius < windowWidth))
-        {
-            circleX += circleSpeed;
-        }
-        if (IsKeyDown(KEY_A) && (circleX + circleRadius > 0))
-        {
-            circleX -= circleSpeed;
-        }
-        if (IsKeyDown(KEY_S) && (circleY + circleRadius < windowHeight))
-        {
-            circleY += circleSpeed;
-        }
-        if (IsKeyDown(KEY_W) && (circleY + circleRadius > 0))
-        {
-            circleY -= circleSpeed;
-        }
-        DrawCircle(circleX, circleY, circleRadius, RED);
+        ClearBackground(WHITE);
 
-        Vector2 desiredAxeDirection{circleX - axeX, circleY - axeY};
-        float directionMagnitude{sqrt(pow(desiredAxeDirection.x, 2) + pow(desiredAxeDirection.y, 2))};
-        Vector2 normalisedAxeDirection{desiredAxeDirection.x / directionMagnitude, desiredAxeDirection.y / directionMagnitude};
-        Vector2 acceleratedAxeDirection{normalisedAxeDirection.x * axeAcceleration, normalisedAxeDirection.y * axeAcceleration};
-        Vector2 temp{acceleratedAxeDirection.x + axeVelocity.x, acceleratedAxeDirection.y + axeVelocity.y};
-        if (sqrt(pow(temp.x, 2) + pow(temp.y, 2)) < axeMaxSpeed)
+        float deltaTime = GetFrameTime();
+
+        if (characterPos.y >= windowHeight - characterRect.height)
         {
-            axeVelocity = temp;
+            velocity = 0;
+            if (IsKeyDown(KEY_SPACE))
+            {
+                velocity = jumpVelocity;
+            }
+        }
+        else
+        {
+            velocity += gravity * deltaTime;
+        }
+        characterPos.y += velocity * deltaTime;
+        animTimeTracker += deltaTime;
+        if (animTimeTracker > animUpdateTime)
+        {
+
+            characterRect.x = frame * characterRect.width;
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+
+            animTimeTracker = 0.0;
         }
 
-        axeX += axeVelocity.x;
-        axeY += axeVelocity.y;
-
-        DrawRectangle(axeX, axeY, axeLength, axeLength, GREEN);
-
-        if (directionMagnitude < (circleRadius + axeLength / 2))
-        {
-            DrawText("Game Over!", 400, 200, 20, RED);
-        }
+        DrawTextureRec(characterTexture, characterRect, characterPos, WHITE);
 
         EndDrawing();
     }
+    UnloadTexture(characterTexture);
+    CloseWindow();
 }
