31d87eb54e4e84dd4ee1c55d4b0298fa7d63a1a2
Matvey Shchetinin <mshchetinin64@gmail.com>
2023-10-12 19:33:55 +0100

    Continues runner

--- test.cpp
+++ test.cpp
@@ -77,6 +77,11 @@ int main()
     const int windowHeight{400};
     InitWindow(windowWidth, windowHeight, "Runner Game");
 
+    Texture2D nebulaTex = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebulaRect{0.0, 0.0, nebulaTex.width / 8, nebulaTex.height / 8};
+    Vector2 nebulaPos{windowWidth, windowHeight - nebulaRect.height};
+    int nebulaVelocity(-200);
+
     Texture2D characterTexture = LoadTexture("textures/scarfy.png");
     Rectangle characterRect;
     characterRect.width = characterTexture.width / 6;
@@ -95,6 +100,10 @@ int main()
     float animTimeTracker{0};
     int frame{0};
 
+    const float nebulaUpdateTime{1.0 / 12.0};
+    float nebulaRunningTime{0};
+    int nebulaFrame{0};
+
     // int posY{windowHeight - rectHeight};
     int velocity{0};
 
@@ -109,6 +118,7 @@ int main()
         if (characterPos.y >= windowHeight - characterRect.height)
         {
             velocity = 0;
+            animTimeTracker += deltaTime;
             if (IsKeyDown(KEY_SPACE))
             {
                 velocity = jumpVelocity;
@@ -118,11 +128,13 @@ int main()
         {
             velocity += gravity * deltaTime;
         }
+
+        nebulaPos.x += nebulaVelocity * deltaTime;
+
         characterPos.y += velocity * deltaTime;
-        animTimeTracker += deltaTime;
+
         if (animTimeTracker > animUpdateTime)
         {
-
             characterRect.x = frame * characterRect.width;
             frame++;
             if (frame > 5)
@@ -133,10 +145,26 @@ int main()
             animTimeTracker = 0.0;
         }
 
+        nebulaRunningTime += deltaTime;
+        if (nebulaRunningTime > nebulaUpdateTime)
+        {
+            nebulaRect.x = nebulaFrame * nebulaRect.width;
+            nebulaFrame++;
+            if (nebulaFrame > 7)
+            {
+                nebulaFrame = 0;
+            }
+
+            nebulaRunningTime = 0.0;
+        }
+
+        DrawTextureRec(nebulaTex, nebulaRect, nebulaPos, WHITE);
+
         DrawTextureRec(characterTexture, characterRect, characterPos, WHITE);
 
         EndDrawing();
     }
     UnloadTexture(characterTexture);
+    UnloadTexture(nebulaTex);
     CloseWindow();
 }
